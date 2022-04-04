b027abe70539e770ebbe80bdb3cca315891ffa85
Betsy Tsoi <tsoisybetsy@gmail.com>
2022-04-04 21:58:57 +0900

    Create a simple UI

---
+++ external.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) {
+        return showErrMsg();
+    }
+    return Math.round(a / b * 1000) / 1000;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return showErrMsg();
+    }
+}
+
+function showErrMsg() {
+    return "ERROR";
+}
